36b3cb89d0689317b06cae84d935bb00e50e1c4a
James <jamesdix@cwgsy.net>
2016-11-09 16:01:39 +0000

    Syncs repo with pair partner

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,31 @@
-require 'bike.rb'
+
 class DockingStation
+
+  attr_reader = :bikes
+
   def release_bike
-     Bike.new
+    Bike.new
+  end
+
+  def dock_bike(bike)
+    if bike.class == Bike
+      @bikes = bike
+      return "Bike Docked"
+    else
+      "That's not a bike"
+    end
+  end
+
+  def bikes
+    @bikes
+  end
+
+
+
+end
+
+class Bike
+  def working?
+    true
   end
 end
