1575e89cee29f777ebbd62b993af8234da94e12b
DarkChocLatte <darkchoclatte@gmail.com>
2016-11-09 17:27:27 +0000

    ex 12 50% complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,21 @@
 
+
 class DockingStation
 
   attr_reader = :bikes
 
+  def initialize
+    @bikes = []
+    5.times{@bikes << Bike.new }
+  end
+
   def release_bike
-    Bike.new
+    @bikes.pop
   end
 
   def dock_bike(bike)
     if bike.class == Bike
-      @bikes = bike
+      @bikes << bike
       return "Bike Docked"
     else
       "That's not a bike"
@@ -17,9 +23,14 @@ class DockingStation
   end
 
   def bikes
-    @bikes
+    @bikes.count
   end
 
+end
 
+class Bike
 
+  def working?
+  true
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,40 +1,44 @@
 require 'docking_station'
 
-
+#
 RSpec.describe DockingStation do
-station = DockingStation.new
-bike = station.release_bike
-a_docked_bike = bike
+# station = DockingStation.new
+# bike = station.release_bike
+# a_docked_bike = bike
+#
+# it {is_expected.to respond_to(:release_bike)}
+# it {is_expected.to respond_to(:dock_bike)}
+# it {expect(bike).to be_truthy}
+# it {expect(a_docked_bike).to eq bike}
+it {expect(:bikes).to be_empty}
 
-it {is_expected.to respond_to(:release_bike)}
-it {is_expected.to respond_to(:dock_bike)}
-it {expect(bike).to be_truthy}
-it {expect(a_docked_bike).to eq bike}
-it {expect { bike }.to raise_error}
 
 end
 
 
 
-RSpec.describe :release_bike do
-  it {is_expected.not_to be_nil}
-end
 
-RSpec.describe "Does Docking station get a bike and does it work?" do
-  station = DockingStation.new
-  bike = station.release_bike
-  bike_status = bike.working?
-
-it {expect(bike).to be_an_instance_of(Bike)}
-it {expect(bike_status).to eq(true)}
-end
-
-RSpec.describe Bike do
-
-  it {is_expected.to respond_to(:working?)}
-
-end
-
-
-
-#Docking station will create an instance of a bike object
+#
+# RSpec.describe :release_bike do
+#   it {is_expected.not_to be_nil}
+#   it {is_expected.to raise_error("ERROR")}
+# end
+#
+# RSpec.describe "Does Docking station get a bike and does it work?" do
+#   station = DockingStation.new
+#   bike = station.release_bike
+#   bike_status = bike.working?
+#
+# it {expect(bike).to be_an_instance_of(Bike)}
+# it {expect(bike_status).to eq(true)}
+# end
+#
+# RSpec.describe Bike do
+#
+#   it {is_expected.to respond_to(:working?)}
+#
+# end
+#
+#
+#
+# #Docking station will create an instance of a bike object
